3f174380344b0385564b7a82680fdca91e59759d
RandyChen <jushanc2@illinois.edu>
2023-02-25 11:57:16 -0600

    modified plotting function in train pendulum

--- train_pendulum.py
+++ train_pendulum.py
@@ -3,11 +3,6 @@ import numpy as np
 from discrete_pendulum import Pendulum
 import matplotlib.pyplot as plt
 
-
-
-
-
-
 class SarsaTD0:
     def __init__(self, env, alpha=0.1, gamma=0.9, epsilon=0.1):
         self.env = env
